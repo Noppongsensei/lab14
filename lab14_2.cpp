489c16d24f0ac6309ec549a1ca0318ca2cf62a5a
Sensei <noppongsen@gmail.com>
2024-01-11 17:53:44 +0700

    14.2

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -19,3 +19,35 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+void showMatrix(const bool A[][N]){
+    for(int j=0;j<N;j++){
+        for(int i=0;i<N;i++){
+            cout<<A[j][i]<<" ";
+            }
+            cout<<"\n";
+        }
+}
+void inputMatrix(double V[][N]){
+    for(int i=0;i<N;i++){
+        cout<<"Row "<<i+1<<": ";
+        for(int j=0;j<N;j++){
+        cin>>V[i][j];
+            }
+        }
+}
+void findLocalMax(const double A[][N], bool B[][N]) {
+    for(int S=0;S<N;S++){
+        B[0][S]=0;
+        B[S][0]=0;
+        B[S][N-1]=0;
+        B[N-1][S]=0;
+        }
+    for (int i = 1; i < N - 1; ++i) {
+        for (int j = 1; j < N - 1; ++j) {
+            B[i][j] = (A[i][j] >= A[i - 1][j]) &&
+                       (A[i][j] >= A[i + 1][j]) &&
+                       (A[i][j] >= A[i][j - 1]) &&
+                       (A[i][j] >= A[i][j + 1]);
+        }
+    }
+}
